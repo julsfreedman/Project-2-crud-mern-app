a15ca2f09fdefc1d82bcc7dc9e1886b1633b8ad8
Julia Freedman <113859369+julsfreedman@users.noreply.github.com>
2023-01-04 17:01:27 -0500

    trying to deploy to render

--- server.js
+++ server.js
@@ -68,7 +68,11 @@ app.put("/update/:id", (req, res) => {
     .catch((err) => console.log(err));
 });
 
-app.get("*", (req, res) => {
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build", "index.html"))
 })
 
